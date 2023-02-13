48045037b50a70418c57f43121b1b24ad6f3959a
Luis Carlos Garcia Gomez <lcgarcia@bancoademi.local>
2023-02-13 18:44:48 -0400

    added style contact

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { useRef } from 'react';
 import emailjs from 'emailjs-com';
-import './contact.css';
 import { motion } from 'framer-motion';
+import {NavTwo} from '../Navbar';
 import {animationThree} from '../Animations/Animations'
+import {BsWhatsapp, BsInstagram} from 'react-icons/bs';
+import {FaPhone} from 'react-icons/fa';
+
 
 function Contact() {
   const form = useRef();
@@ -23,16 +26,29 @@ function Contact() {
     
 
   return (
-    <motion.div initial='out' animate='in' exit='out' variants={animationThree}>
-    <div>Contact</div>
-    <form ref={form} onSubmit={sendEmail}>
-        <input type="text" name='name' placeholder='Your Name' required/>
-        <input type="email" name='email' placeholder='Your Email' required/>
-        <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
-        <button type='submit' className='btn btn-primary'>Send Message</button>
-
+    <>
+    <NavTwo />
+    <motion.div initial='out' animate='in' exit='out' variants={animationThree} className='h-screen'>
+    
+    <section className='flex items-center justify-center content-center'>
+    
+    <div className='w-[50%] h-full items-center justify-center content-center mx-3'>
+    <div className='p-20 border-solid rounded-[15px] border-2 m-3'><BsWhatsapp /></div>
+    <div className='p-20 border-solid rounded-[15px] border-2 m-3'><BsInstagram /></div>
+    <div className='p-20 border-solid rounded-[15px] border-2 m-3'><FaPhone /></div>
+    </div>
+    <div className='w-[100%] h-screen flex items-center justify-center content-center'>
+      <form ref={form} onSubmit={sendEmail} className='flex-col content-center items-center justify-center'>
+        <input type="text" name='name' placeholder='Escribe tu nombre' className='w-full p-2 m-2 rounded-[15px]' required/>
+        <input type="email" name='email' placeholder='Escribe tu correo electronico' className='w-full p-2 m-2 rounded-[15px]' required/>
+        <textarea name="message" rows="7" placeholder='Escribe tu mensaje' className='w-full p-2  m-2 rounded-[15px]' required></textarea>
+        <button type='submit' className='btn btn-primary p-2'>Enviar Mensaje</button>
       </form>
+      </div>
+  
+    </section>
       </motion.div>
+    </>
   )
 }
 
